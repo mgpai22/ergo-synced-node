cd516250103f795a49053b1f05ae8d2408364cac
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-07-25 21:07:35 -0400

    add conf parser, use only ipv4 addresses

--- ergo-node/python-conf/Operations.py
+++ ergo-node/python-conf/Operations.py
@@ -2,22 +2,22 @@ import json
 import requests
 import asyncio
 import aiohttp
+from aiohttp import ClientSession
+from pyhocon import ConfigFactory, HOCONConverter
+from typing import List, Dict, Any
 
 
-def writeToConf(peerList):
-    data = ['ergo {', '    directory = "/ergo/.ergo"', '    node {mining = false}',
-            '    wallet.secretStorage.secretDir = ${ergo.directory}"/wallet/keystore"', '}', 'scorex {',
-            '    restApi {',
-            '        apiKeyHash = "324dcf027dd4a30a932c441f365a25e86b173defa4b8e58948253471b81b72cf"', '}',
-            'network {', '    knownPeers = ' + str(json.dumps(peerList)), '    maxConnections = ' + str(len(peerList)),
-            '}', '}']
-    with open('ergo.conf', 'w') as conf:
-        for line in data:
-            conf.write(line)
-            conf.write('\n')
+def writeToConf(peerList: List[str], conf_file: str) -> None:
+    conf = ConfigFactory.parse_file(conf_file)
+    knownPeers = conf.get_list('scorex.network.knownPeers')
+    updatedPeers = knownPeers + peerList
+    conf.put('scorex.network.knownPeers', updatedPeers)
+    conf_string = HOCONConverter.to_hocon(conf)
+    with open('ergo.conf', 'w') as f:
+        f.write(conf_string)
 
 
-def write(new_data, filename):  # function that appends data to the specified file
+def write(new_data, filename: str) -> None:  # function that appends data to the specified file
     with open(filename, "r") as file1:
         data = json.load(file1)
         data.append(new_data)
@@ -27,12 +27,12 @@ def write(new_data, filename):  # function that appends data to the specified fi
         json.dump(data, file1, indent=4)
 
 
-def file(x):  # Function to load specified file
-    with open(x, 'r+') as file:
+def file(file_name: str):  # Function to load specified file
+    with open(file_name, 'r+') as file:
         return json.load(file)
 
 
-def clear(file):  # function to clear the file so appended data isn't constantly repeated
+def clear(file: str) -> None:  # function to clear the file so appended data isn't constantly repeated
     dict1 = []  # After everything is cleared this is what will be put back into the fresh  file
     out_file = open(file, "w")
 
@@ -41,12 +41,11 @@ def clear(file):  # function to clear the file so appended data isn't constantly
     out_file.close()
 
 
-def NodeAPIcall(ip):
+def NodeAPIcall(ip: str) -> Dict[str, Any]:
     URL = f'http://{ip}/peers/syncInfo'
     return requests.get(url=URL).json()
 
-
-async def get(url, session):
+async def get(url: str, session: ClientSession) -> List[Dict[str, Any]]:
     URL = f'http://{url}/peers/syncInfo'
     data = []
     try:
@@ -58,7 +57,7 @@ async def get(url, session):
     return data
 
 
-async def main(urls):
+async def main(urls: List[str]):
     async with aiohttp.ClientSession() as session:
         resp = await asyncio.gather(*[get(url, session) for url in urls])
     return resp
--- ergo-node/python-conf/port.py
+++ ergo-node/python-conf/port.py
@@ -1,16 +1,39 @@
+import ipaddress
 import socket
-from ipaddress import ip_address
 
 
-def splitIP(ipAddr):
+def splitIP(ipAddr: str) -> str:
     ip, separator, port = ipAddr.rpartition(':')
     assert separator  # separator (`:`) must be present
     port = int(port)  # convert to integer
-    ip = ip_address(ip.strip("[]"))  # convert to `IPv4Address` or `IPv6Address`
+    ip = ipaddress.ip_address(ip.strip("[]"))  # convert to `IPv4Address` or `IPv6Address`
     return ip
 
 
-def portCheck(ip, port):
+def ip_address_isIPV4(ip_with_port: str) -> bool:
+    try:
+        ip, port = ip_with_port.split(':')
+        port = int(port)
+        if not (1 <= port <= 65535):
+            return False
+    except ValueError:
+        return False
+
+    try:
+        ipaddress.IPv4Address(ip)
+        return True
+    except ipaddress.AddressValueError:
+        pass
+    try:
+        ipaddress.IPv6Address(ip)
+        return False
+    except ipaddress.AddressValueError:
+        pass
+
+    return False
+
+
+def portCheck(ip: str, port: int) -> str:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1.0)
     result = sock.connect_ex((ip, port))
