5dab6594836f1d28648d37f7058ede0145c02448
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-07-25 21:25:34 -0400

    update for use with docker compose plug-in

--- ergo-node/updatePeerList.sh
+++ ergo-node/updatePeerList.sh
@@ -1,9 +1,8 @@
-sudo apt update
 cd python-conf
-docker-compose up 
+docker compose up
 cd ..
 cp python-conf/ergo.conf ergo.conf
-docker-compose down
+docker compose down
 echo node shutdown to load changes
-docker-compose up --build -d
+docker compose up --build -d
 echo The node is back up! Changes are loaded.
